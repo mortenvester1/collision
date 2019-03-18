c6c28d4d41c627c45e8b4d9f501afee321f3bd41
Morten V. Pedersen <morten.pedersen@ibm.com>
2019-03-18 11:33:39 -0400

    now with argparse

--- collision/simulate.py
+++ collision/simulate.py
@@ -6,20 +6,45 @@ from engine import *
 from objects import *
 
 
-def main():
-    left =  Block(mass=100**0, shape=(1,1), start_v=0., start_x=2)
-    right =  Block(mass=100**2., shape=(1,1), start_v=-1, start_x=4)
+def main(dt, ratio, v, viz):
+
+
+    left =  Block(mass=100**0, shape=(1,1), start_v=0., start_x=3)
+    right =  Block(mass=100**ratio, shape=(1,1), start_v=v, start_x=6)
     wall = Wall(position=0, width=1)
 
-    engine = PhysicsEngine(timestep=1e-2,
+    engine = PhysicsEngine(timestep=dt,
                            wall=wall,
                            left_square=left,
-                           right_square=right,
-                           visual=False)
+                           right_square=right)
 
-    engine.run(debug=False)
+    engine.run(debug=False, viz=viz)
     return
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+
+    # action=store_true means default is False
+    parser.add_argument('-V', '--viz',
+        action='store_true',
+        help='flag whether to visualize')
+    parser.add_argument('-dt', '--timestep',
+        type=float,
+        default=1e-2,
+        help='size of time step')
+    parser.add_argument('-r', '--ratio',
+        type=int,
+        default=3,
+        help='mass ratio')
+    parser.add_argument('-v', '--velocity',
+        type=float,
+        default=-1,
+        help='init velocity')
+
+    args = parser.parse_args()
+    dt = args.timestep
+    ratio = args.ratio
+    viz = args.viz
+    v = args.velocity
+    main(dt=dt, ratio=ratio, v=v, viz=viz)

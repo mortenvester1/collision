2cd71b355062e36f0d6693de55734e985a1f6fbc
Morten V. Pedersen <morten.pedersen@ibm.com>
2019-03-18 11:33:56 -0400

    now with visualization. Now auto close though

--- collision/engine.py
+++ collision/engine.py
@@ -1,6 +1,9 @@
-from fractions import Fraction
-import numpy as np
 import pdb
+import time
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
 
 
 class PhysicsEngine(object):
@@ -9,14 +12,15 @@ class PhysicsEngine(object):
     simple 2d world. The world has an immovable wall on the left side
     and extends into the void.
     """
-    def __init__(self, timestep=1e-6, wall=None, left_square=None, right_square=None, visual=False):
+    def __init__(self, timestep=.2, wall=None, left_square=None, right_square=None):
         """
         Initialize the world
 
         visualize
         """
-        assert visual is False, "Visualization not yet implemented"
-        self.t = timestep
+        #assert viz is False, "Visualization not yet implemented"
+        self.dt = timestep
+        self.elapsed = 0
         self.l = left_square
         self.r = right_square
         self.w = wall
@@ -24,8 +28,17 @@ class PhysicsEngine(object):
 
         self.eps = 1e-6
 
+    def run(self, viz=False, verbose=True, debug=True):
+        if viz:
+            self._run_ani()
+            #raise Exception('viz not ready')
+        else:
+            self._run(verbose=verbose, debug=debug)
 
-    def run(self, verbose=True, debug=True):
+        return
+
+
+    def _run(self, verbose=True, debug=True):
         """
         run the full simulation until no more collisions occur
         """
@@ -35,18 +48,20 @@ class PhysicsEngine(object):
 
         if verbose: print("{0:>8s}\t{1:>11s}\t{2:>7s}\t\t{3:>7s}\t{4:>10s}\t{5:>10s}\t{6:>10s}"\
                            .format("time", "#Collisions", "left x", "right x", "left v", "right v", "total K"))
+
         prev_count = -1
         prev_K = self.l.K + self.r.K
         while True and not end_simulation:
             #Take a timestep
-            elapsed += self.timestep(self.w, self.l, self.r)
+            self.elapsed += self._step(self.w, self.l, self.r)
 
             # Compute and print metrics/stats/...
             new_K = self.l.K + self.r.K
             dK = abs(prev_K - new_K)
-            if verbose and prev_count < self.c:
+            #if verbose and prev_count < self.c:
+            if verbose:
                 print("{0:>8.4f}\t{1:>11d}\t{2:>7.4f}\t\t{3:>7.4f}\t{4: >10.4f}\t{5: >10.4f}\t{6:>10.4f}"\
-                   .format(elapsed, self.c, self.l.x, self.r.x, self.l.v, self.r.v, dK), flush=True)
+                   .format(self.elapsed, self.c, self.l.x, self.r.x, self.l.v, self.r.v, dK), flush=True)
                 prev_count = self.c
 
             # Force simulation to end when no more collisions
@@ -66,13 +81,86 @@ class PhysicsEngine(object):
 
         return
 
-    def timestep(self, wall, lblock, rblock, dt=None):
+
+    def _run_ani(self):
+        xlim_r = self.r.x + self.r.w/2 + 1
+        xlim_l = self.w.x - self.w.w/2 - 1
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                             xlim=(xlim_l, xlim_r), ylim=(0, 4))
+        ax.grid()
+
+        wall, = ax.fill([], [], color='k')
+        lblock, = ax.fill([], [], color='b')
+        rblock, = ax.fill([], [], color='r')
+        time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+        count_text = ax.text(0.02, 0.90, '', transform=ax.transAxes)
+
+        def draw_square(x, w, h=1):
+            arr = np.array([[x - w/2, 0], [x + w/2, 0],
+                            [x + w/2, h], [x - w/2, h]])
+            return arr
+
+
+        def animate(frame):
+            """ update animate animation step """
+            wall.set_xy(draw_square(self.w.x, self.w.w, h=2))
+            lblock.set_xy(draw_square(self.l.x, self.l.w, self.l.w))
+            rblock.set_xy(draw_square(self.r.x, self.r.w, self.r.w))
+
+            time_text.set_text('time: {0:5.4f}'.format(self.elapsed))
+            count_text.set_text('count: {0:<d}'.format(self.c))
+
+            return wall, lblock, rblock, time_text, count_text
+
+
+        def frames_generator():
+            """
+            generate
+            """
+            while True:
+                dt = self._ani_step(wall=self.w, lblock=self.l, rblock=self.r)
+                yield dt
+                if dt is None:
+                    time.sleep(1)
+                    break
+                self.elapsed += dt
+
+        try:
+            meh = ani.FuncAnimation(
+                fig,
+                animate,
+                frames=frames_generator,
+                interval=0,
+                blit=True,
+                init_func=None,
+                repeat=False)
+            plt.show(block=True)
+            #meh.event_source.stop()
+            #del meh
+            #plt.close()
+        except:
+            pass
+
+        return
+
+
+    def _ani_step(self, wall, lblock, rblock, dt=None):
+
+        if self.check_more_collisions(lblock, rblock):
+            return None
+
+        return self._step(wall, lblock, rblock)
+
+
+    def _step(self, wall, lblock, rblock, dt=None):
         """
         take a single timestep, if a collisino occurs in that timestep, we will
         set the positions to where the colliion occurs, update speed
         """
         if dt is None:
-            dt = self.t
+            dt = self.dt
 
         # 0 velocity change if no collision
         l_dv, r_dv = 0, 0
@@ -104,6 +192,7 @@ class PhysicsEngine(object):
         # no collision we just update
         self.update(l_new_x, l_new_v, r_new_x, r_new_v)
         return dt
+
 
     def check_more_collisions(self, lblock, rblock):
         """
